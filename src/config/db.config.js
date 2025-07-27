6fbd462c84a9de8900ceea44c6e2eb33b7151ca3
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2025-07-27 14:29:23 +0530

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,47 +1,47 @@
 const env = process.env.NODE_ENV || "development";
 
 const config = {
-    development: {
-        HOST: "localhost",
-        USER: "root",
-        PASSWORD: "",
-        DB: "certed_dev",
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+  development: {
+    HOST: "srv1641.hstgr.io",
+    USER: "u844961577_certed_learn",
+    PASSWORD: "Latyton@#1993",
+    DB: "u844961577_certed_learn",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
+  },
 
-    test: {
-        HOST: "localhost",
-        USER: "root",
-        PASSWORD: "",
-        DB: "",
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+  test: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
+  },
 
-    production: {
-        HOST: "",
-        USER: "root",
-        PASSWORD: "",
-        DB: "certed",
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+  production: {
+    HOST: "",
+    USER: "certed_admin",
+    PASSWORD: "10#%pRg@5",
+    DB: "certed",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
+  },
 };
 
 module.exports = config[env];
